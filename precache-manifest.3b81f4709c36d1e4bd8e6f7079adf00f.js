ec7aa5753ad22c265405f00af5d1a3ce00ea1ba1
yogeshpm8910@gmail.com <yogeshpm8910@gmail.com>
2020-03-18 20:30:49 +0530

    Updates

---
+++ precache-manifest.3b81f4709c36d1e4bd8e6f7079adf00f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2b043a778f2c10a4b5aabc0ea72567",
+    "url": "/contact-manager-redux/index.html"
+  },
+  {
+    "revision": "f963f1b6e54d9c0ce337",
+    "url": "/contact-manager-redux/static/css/2.725f6fc8.chunk.css"
+  },
+  {
+    "revision": "f8a909f52d5fd40a2fc2",
+    "url": "/contact-manager-redux/static/css/main.b1efc8f5.chunk.css"
+  },
+  {
+    "revision": "f963f1b6e54d9c0ce337",
+    "url": "/contact-manager-redux/static/js/2.5371cecc.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/contact-manager-redux/static/js/2.5371cecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a909f52d5fd40a2fc2",
+    "url": "/contact-manager-redux/static/js/main.cad99899.chunk.js"
+  },
+  {
+    "revision": "cc19ddedda180e0cc84d",
+    "url": "/contact-manager-redux/static/js/runtime-main.97d7644d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contact-manager-redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contact-manager-redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contact-manager-redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contact-manager-redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contact-manager-redux/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
